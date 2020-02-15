af860e992aa61a4a31219ec9965f31c10edcaf9b
Rick Belson <belsonrj@aol.com>
2020-02-15 23:53:46 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,29 +1,3 @@
-Skip to content
-Search or jump to…
-
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@belsonrj 
-Learn Git and GitHub without any code!
-Using the Hello World guide, you’ll start a branch, write comments, and open a pull request.
-
-
-alexisadorn
-/
-Flatiron-OO-Ruby
-1
-014
- Code Issues 0 Pull requests 0 Actions Projects 0 Wiki Security Insights
-Flatiron-OO-Ruby/oo-cash-register-v-000/lib/cash_register.rb / 
-@alexisadorn alexisadorn oo-cash-register-v-000
-1ebac13 on May 17, 2017
-39 lines (34 sloc)  748 Bytes
-  
-You're using code navigation to jump to definitions or references.
-Learn more or give us feedback
 require 'pry'
 class CashRegister
   attr_accessor :total, :discount, :price, :items
@@ -63,4 +37,3 @@ class CashRegister
   end
 
 end
-
